1c7c66ec81a7d05bae92bd3b9ef609f18392cb8b
Manoj221995 <147677182+Manoj221995@users.noreply.github.com>
2025-03-15 12:24:24 +0530

    Add files via upload

---
+++ Testingbike.java
@@ -0,0 +1,33 @@
+package Inheritance;
+
+public class Testingbike {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Bikedemo obj = new Bikedemo();
+		
+		obj.startbutton();
+		
+		obj.stopbutton();
+		
+		obj.fueltank();
+		
+	 Pulsar obj1 = new Pulsar();
+		obj1.  colour();
+	
+		obj1.startbutton();
+		
+	   Yamaha obj2= new Yamaha();
+		 
+		obj2 . startbutton();
+		
+		obj2 . stopbutton();
+		
+		obj2 . fueltank ();
+		 
+		obj2.  future();
+	}
+
+}
